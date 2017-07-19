034072e97a49e0ced595a90214c675d6b778def5
Zenith Zinc <admin@znzinc.net>
2017-07-19 13:05:05 +0900

    Notice feature added

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # This bot uses python 3.6
 
-# Ver 0.9.0
+# Ver 1.0.0
 
 import json
 import os
 import time
 from datetime import date
+from datetime import datetime
 
 import discord
 
@@ -14,7 +15,10 @@ keyFile = open("./keys.json", "r")
 key = json.loads(keyFile.read())
 keyFile.close()
 
-botCommands = ["!주사위", "!선택", "!도움말", "!제작정보", "!판매정보"]
+file = open("./channels.txt", "r")
+tmp = file.readlines()
+channel_list = [x.strip() for x in tmp]
+file.close()
 
 
 def inputLogger(message):
@@ -25,27 +29,72 @@ def inputLogger(message):
     logfile.close()
 
 
+def noticeLogger(content):
+    # input : normal string
+    logfile = open("./notice_log.log", mode="a", newline="\r\n")
+    logfile.write("[" + str(time.strftime("%Y-%m-%d %H:%M:%S")) + ", sent to " + str(len(channel_list)) + " channels]"
+                  + content + "\n")
+    logfile.close()
+
+
+def addToList(ID):
+    global channel_list
+
+    channel_list.append(ID)
+    listFile = open("./channels.txt", "a")
+    listFile.write(ID + "\n")
+
+
+def deleteFromList(IDs):
+    global channel_list
+
+    for ID in IDs:
+        channel_list.remove(ID)
+    listFile = open("./channels.txt", "w")
+    for item in channel_list:
+        listFile.write("%s\n" % item)
+    listFile.close()
+
+
+def isInList(ID):
+    global channel_list
+
+    try:
+        channel_list.index(ID)
+        return True
+    except:
+        return False
+
+
+botCommands = ["!주사위", "!선택", "!도움말", "!제작정보", "!판매정보"]
+adminCommands = ["!공지전송"]
+
+
 @bot.event
 async def on_ready():
-    await bot.change_presence(game=discord.Game(name='FINAL FANTASY XIV'))
-    print("Discord bot logged in as " + bot.user.name + "(" + bot.user.id + ")")
+    await bot.change_presence(game=discord.Game(name='ff14.tar.to'))
+    now = datetime.now()
+    timestr = str(now)
+    print("Discord bot logged in as " + bot.user.name + "(" + bot.user.id + ") at " + timestr)
     print("------------")
-
-async def serverRegister():
-    pass
 
 
 @bot.event
 async def on_message(message):
     try:
-        if message.author.bot:
+        if message.author == bot.user:
             inputLogger(message)
             return
         cmd = message.content.split(" ")[0]
         if cmd in botCommands:
+            if not isInList(message.channel.id):
+                addToList(message.channel.id)
             inputLogger(message)
             await bot.process_commands(message)
             return
+        if cmd in adminCommands and message.author.id == key["admin"]:
+            inputLogger(message)
+            await bot.process_commands(message)
 
     except:
         logfile = open(os.path.join("bot log", str(date.today()) + ".log"), mode="a", newline="\r\n")
@@ -53,7 +102,23 @@ async def on_message(message):
         logfile.close()
 
 
+@bot.command(name="공지전송", pass_context=True)
+async def sendnotice(ctx, *args):
+    global channel_list
+    del_list = []
+    content = " ".join(args)
+    for channel in channel_list:
+        try:
+            await bot.send_message(discord.Object(channel), "*[관리자 공지] " + content + "*")
+        except:
+            del_list.append(channel)
+    if not len(del_list) == 0:
+        deleteFromList(del_list)
+
+    noticeLogger(content)
+
+
 if not os.path.exists("./bot log"):
     os.makedirs("./bot log")
-print("Starting discordbot-ffxiv...")
+print("Starting FFXIV-ZnBot...")
 bot.run(key["bot_token"])
